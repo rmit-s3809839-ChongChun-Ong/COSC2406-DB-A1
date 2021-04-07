a3ff4c3d688432f7c1d224bcad19ecd5f273760f
rmit-s3809839-ChongChun-Ong <81905235+rmit-s3809839-ChongChun-Ong@users.noreply.github.com>
2021-04-07 23:21:26 +1000

    Clean up dbquery file
    Code reviewed and redundant codes have been removed.

--- dbquery.java
+++ dbquery.java
@@ -23,85 +23,8 @@ public class dbquery {
    static final int sNameMaxLength = 50;
    static final int hourlyCountsMaxLength = 4;
  
-
-   public static void SearchText_old(String text, int pagesize){
-      File file = new File("heap" + "." + pagesize);
-      
-      try {
-          InputStream is = new BufferedInputStream(new FileInputStream(file));
-          byte[] buffer = new byte[pagesize];
-          
-          String in;
-          int pageCount = 0;
-          int count = 0;
-          
-          System.out.println("Buffer length:" + buffer.length);
-
-          while(is.read(buffer) != -1){
-              in = new String(buffer);
-              
-              System.out.println("in str:" + in);
-
-              System.out.print("Searching in page " + pageCount + "\r");
-              
-              String[] page = in.split("\r\n");
-              for (int i = 0; i < count; i += 184) {
-              
-            }
-              
-              System.out.println(page.length);
-
-              for(int j=0; j<page.length-1; j++){
-                  String[] token = page[j].split("\t");
-                 
-                  
-                  for(int i=0; i<token.length; i++){
-                  
-                     System.out.println(token[i]);
-
-                     //split the datainto 2 part
-                      String[] data = token[i].split(":",2);
-                      
-                      //at this point, only search in BN_NAME
-                      if(data[0].equals("BN_NAME")){
-                          String check1 = text.toLowerCase();
-                          String check2 = data[1].toLowerCase();
-                          
-                          if(check2.contains(check1)){
-                              System.out.print("Found '" + text + "' in page " + pageCount + ": ");
-                              System.out.print(data[1] + "\r\n");
-                              count++;
-                          }
-                      }
-                  }
-              }
-              pageCount++;
-          }
-          
-          System.out.println();
-          System.out.println("Finish loading.");
-          System.out.println(count + " result(s) found");
-          
-      } catch (FileNotFoundException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-      } catch (IOException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-      }
-      
-  }
-  private static String recordField(byte[] record, int offset, int length) {
-   String field = new String(record, offset, length, StandardCharsets.ISO_8859_1);
-
-   // Use ASCII NUL as string terminator:
-   int pos = field.indexOf('\u0000');
-   if (pos != -1) {
-       field = field.substring(0, pos);
-   }
-
-   return field.trim(); // Trim also spaces for fixed fields.
-}
+   //Search the text from the input arguments
+   //Field bytes will be retrieved based on range position determined by the field length
    public static void SearchText(String text, int pageSize) throws IOException {
       RandomAccessFile objFile = new RandomAccessFile("heap." + pageSize, "r");
 
@@ -117,60 +40,6 @@ public class dbquery {
 objFile.seek(161);
 byte[] objs = new byte[161];
 objFile.read(objs);
-byte[] vcc = Arrays.copyOfRange(objs, 0, 25);
-String strasddd = new String(vcc);
-//System.out.println("text:" + strasddd.trim());
-
-byte[] tmpid = Arrays.copyOfRange(objs, 25, 32);
-int tmpidVal= java.nio.ByteBuffer.wrap(tmpid).getInt();
-//String strid = String.valueOf(tmpid);
-//strid = strid.replaceAll("[\\D]", "");
-//int mds = Integer.parseInt(tmpid);
-//System.out.println("idValue:" + tmpidVal);
-
-byte[] vdt = Arrays.copyOfRange(objs, 32, 55);
-String strdt = new String(vdt);
-//System.out.println("text:" + strdt);
-
-byte[] yr = Arrays.copyOfRange(objs, 55, 77);
-int inyr = java.nio.ByteBuffer.wrap(yr).getInt();
-String stryr = new String(yr);
-//System.out.println("year:" + stryr);
-
-byte[] vdt2 = Arrays.copyOfRange(objs, 77, 85);
-String strdt2= new String(vdt2);
-//System.out.println("month:" + strdt2);
-
-byte[] vmd = Arrays.copyOfRange(objs, 85, 90);
-int mDValue = java.nio.ByteBuffer.wrap(vmd).getInt();
-//String strmdt = new String(vmd);
-//System.out.println("mdate:" + mDateValue);
-
-byte[] mn = Arrays.copyOfRange(objs, 90, 99);
-String strmn= new String(mn);
-//String strmdt = new String(vmd);
-//System.out.println("day:" + strmn);
-
-byte[] tm = Arrays.copyOfRange(objs, 99, 103);
-int intm = java.nio.ByteBuffer.wrap(tm).getInt();
-String strmdt = new String(tm);
-//System.out.println("time:" + intm);
-
-
-byte[] sid = Arrays.copyOfRange(objs, 103, 107);
-int intsid = java.nio.ByteBuffer.wrap(sid).getInt();
-String strsid = new String(sid);
-//System.out.println("sid:" + intsid);
-
-byte[] sname = Arrays.copyOfRange(objs, 107, 157);
-String strsname= new String(sname);
-//String strmdt = new String(vmd);
-//System.out.println("sname:" + strsname);
-
-byte[] hc = Arrays.copyOfRange(objs, 157, 161);
-int inthc = java.nio.ByteBuffer.wrap(hc).getInt();
-String strhc = new String(hc);
-//System.out.println("hc:" + inthc);
     
       for (int i = 0; i < count; i += 161) {
         
@@ -201,8 +70,6 @@ String strhc = new String(hc);
        String strSDTname = new String(SDT_Name);
     
          if(strSDTname.toLowerCase().contains(text.toLowerCase())) {
-        // if(recordCount == 1) {
-           // recordCount = 1;
             System.out.println("Record: " + recordCount);
             System.out.println("Page: " + pageNum);
             System.out.println("SDT_Name: " + strSDTname);
@@ -225,56 +92,17 @@ String strhc = new String(hc);
                recordCount=0;
             }
          }
-     // }
-     
+        
     }
     objFile.close();
    }
    
-   //Validates user input so that page size is a numeric value
-   public static int GetPageSize(String[] args) throws Exception {
-      
-      int position = args.length - 1;
-      try {
-         int posVal = Integer.parseInt(args[position]);
-         return posVal;
-      } catch (Exception e) {
-         System.out.println("Page size must be a number.");
-         System.exit(0);
-      }
-
-      return 0;
-   }
-
-   //Validates the input text whether it is null. 
-   // public static String ValidateString(String[] args) throws Exception {
-
-   //    if (!args[0].isEmpty()) {
-   //       String[] arrName = new String[args.length-1];
-   //       for (int i = 0; i < args.length - 1; i++) {
-   //          arrName[i] = args[i];
-   //       }
-   //       String fullString = String.join(" ", arrName);
-   //       return fullString;
-   //    } else {
-   //       System.out.println("Please enter text for searching.");
-   //       System.exit(0);
-   //    }
-   //    return null;
-   // }
-
    //Calculate the time taken to search the text in the heap file.
    public static void main(String[] args) {
       long intStartTime = System.nanoTime();
       String text="";
       int intPageSize=0;
-      // try {
-      //    int intPageSize = GetPageSize(args);
-      //    String text = ValidateString(args);
-      //    SearchText(text, intPageSize);
-      // } catch (Exception e) {
-      //    e.printStackTrace();
-      // }
+     
     try{
       try{
          text = args[0];
